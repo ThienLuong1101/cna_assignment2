0bfedff53358befb7fc11e20bb70b125982601e9
luongthien <luongthien1236@gmail.com>
2025-05-05 04:28:24 +0930

    3 and 6 testing

--- sr.c
+++ sr.c
@@ -255,7 +255,6 @@ static int B_nextseqnum;   /* the sequence number for the next packets sent by B
 static struct pkt rcv_buffer[WINDOWSIZE];  /* buffer for out-of-order packets */
 static int buffer_status[WINDOWSIZE];      /* track if buffer position is occupied */
 static int rcv_base;                       /* base of receive window */
-
 void B_input(struct pkt packet)
 {
   struct pkt sendpkt;
@@ -294,24 +293,14 @@ void B_input(struct pkt packet)
       
       /* If this is the expected packet, deliver it and consecutive buffered packets */
       if (packet.seqnum == expectedseqnum) {
-        /* Deliver this packet immediately if it is at the base of the window */
         tolayer5(B, packet.payload);
-        
-        /* Check if we need to shift buffer */
-        if (buffer_index == 0) {
-          /* Shift buffer */
-          for (i = 0; i < WINDOWSIZE - 1; i++) {
-            rcv_buffer[i] = rcv_buffer[i + 1];
-            buffer_status[i] = buffer_status[i + 1];
-          }
-          buffer_status[WINDOWSIZE - 1] = 0;
-          rcv_base = (rcv_base + 1) % SEQSPACE;
-        }
+        buffer_status[buffer_index] = 0;
+        rcv_base = (rcv_base + 1) % SEQSPACE;
         expectedseqnum = (expectedseqnum + 1) % SEQSPACE;
-        
-        /* Deliver consecutive buffered packets */
-        while (buffer_status[0] == 1) {
-          tolayer5(B, rcv_buffer[0].payload);
+   
+        rel_seqnum = 0; /* Check position 0 of buffer array */
+        while (buffer_status[rel_seqnum] == 1) {
+          tolayer5(B, rcv_buffer[rel_seqnum].payload);
           
           /* Shift buffer */
           for (i = 0; i < WINDOWSIZE - 1; i++) {
@@ -339,8 +328,7 @@ void B_input(struct pkt packet)
     sendpkt.acknum = packet.seqnum;
   }
   else {
-   
-    /* do not send ACK for corrupted packet */
+  
     return;
   }
 
